8544a42ed7bf5cb8151f781fd02bd252b74e6893
bindi-v <bindi.vaghela@gmail.com>
2021-10-03 16:24:05 -0400

    display day and date

--- script.js
+++ script.js
@@ -78,11 +78,11 @@ let dateFive = moment().add(120, 'hours').format('MMM DD');
 window.onload = function displayLastSearch() {
     if (localStorage.getItem('lastCity') === null) {
         let city = 'Atlanta'
-        getWeather(city)
+        getCityWeather(city)
     }
     else {
         let city = localStorage.getItem('lastCity') 
-        getWeather(city)
+        getCityWeather(city)
     }
 }
 
@@ -92,8 +92,136 @@ document
         event.preventDefault()
         let city = document.getElementById('city').value
         let savedCities = document.getElementById('savedCities')
-        getWeather(city)
+        getCityWeather(city)
         savedCities.insertAdjacentHTML("afterbegin", `<button type="button" class="button is-fullwidth mb-1" cityName="${city}" onclick="pushCity(this)">${city}</button>`)
         localStorage.setItem('lastCity', city)
-        document.getElementById('searchError').textContent = ""
+        document.getElementById('searchError').textContent = "Something Wrong!"
     })
+
+    // When a saved city button is pressed, display weather for that city.
+function pushCity(button) {
+    let city = button.getAttribute('cityName')
+    getCityWeather(city)
+}
+
+function getCityWeather (city) {
+    let longlatURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&cnt=1&appid=7e44ab7dc38056f61c9d41fc361df519`
+
+    return fetch(longlatURL)
+        .then(function (response) {
+return response.json()
+        })
+        .then(function (longlatResults) {
+            console.log(longlatResults)
+            let lon = longlatResults.city.coord.lon
+            let lat = longlatResults.city.coord.lat
+
+            let weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=7e44ab7dc38056f61c9d41fc361df519`
+
+            return fetch(weatherURL)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+
+        //cityName = data.name;
+        //cityMinTemp = data.main.temp_min;
+       // cityMaxTemp = data.main.temp_max;
+       // tempIcon = data.weather[0].icon;
+        //cityWind = data.wind.speed;
+        //cityHumidity = data.main.humidity;
+        
+            // Today's weather.
+            document.getElementById('cityName').innerHTML = `
+            ${city} <img src="images/${data.current.weather[0].icon}.svg" style="width:50px;height50px;" class="has-image-centered" alt="${data.current.weather[0].description} weather icon.">`
+
+            document.getElementById('date').innerHTML = `${today}`
+
+            document.getElementById('currentTemp').innerHTML = `
+            <text class="title is-0">${Math.round(data.current.temp)} F</text>`
+
+            document.getElementById('currentHumidity').innerHTML = `
+            <text class="title is-0">${data.current.humidity}%</text>`
+
+            document.getElementById('currentWind').innerHTML = `
+            <text class="title is-0">${data.current.wind_speed}</text><text class="title is-3">mph</text>`
+
+            
+
+            // 5 Day Forecast Cards
+            //Tomorrow's weather
+            document.getElementById('imgOne').innerHTML = `
+            <img src="images/${data.daily[0].weather[0].icon}.svg" style="width:50px;height50px;" class="pt-2 pb-2" alt="${data.daily[0].weather[0].description} weather icon.">`
+
+            document.getElementById('tomorrow').innerHTML = `<strong>${tomorrow}</strong>`
+
+            document.getElementById('dateTomorrow').innerHTML = `<p class="has-text-grey has-text-centered mb-1">${dateTomorrow}</p>`
+
+            document.getElementById('tomorrowTemp').innerHTML = `Max Temp :
+            ${Math.round(data.daily[0].temp.max)}  Min Temp : ${Math.round(data.daily[0].temp.min)} F`
+
+            document.getElementById('tomorrowHumidity').innerHTML = `
+            H: ${data.daily[0].humidity}%`
+
+            // Day 2's weather
+            document.getElementById('imgTwo').innerHTML = `
+            <img src="images/${weatherResults.daily[1].weather[0].icon}.svg" style="width:50px;height50px;" class="pt-2 pb-2" alt="${weatherResults.daily[1].weather[0].description} weather icon.">`
+
+            document.getElementById('dayTwo').innerHTML = `<strong>${dayTwo}</strong>`
+
+            document.getElementById('dateTwo').innerHTML = `<p class="has-text-grey has-text-centered mb-1">${dateTwo}</p>`
+
+            document.getElementById('dayTwoTemp').innerHTML = `
+            ${Math.round(weatherResults.daily[1].temp.max)} / ${Math.round(weatherResults.daily[1].temp.min)} F`
+
+            document.getElementById('dayTwoHumidity').innerHTML = `
+            H: ${weatherResults.daily[1].humidity}%`
+
+            // Day 3's weather
+            document.getElementById('imgThree').innerHTML = `
+            <img src="images/${weatherResults.daily[2].weather[0].icon}.svg" style="width:50px;height50px;" class="pt-2 pb-2" alt="${weatherResults.daily[2].weather[0].description} weather icon.">`
+            
+            document.getElementById('dayThree').innerHTML = `<strong>${dayThree}</strong>`
+
+            document.getElementById('dateThree').innerHTML = `<p class="has-text-grey has-text-centered mb-1">${dateThree}</p>`
+
+            document.getElementById('dayThreeTemp').innerHTML = `
+            ${Math.round(weatherResults.daily[2].temp.max)} / ${Math.round(weatherResults.daily[2].temp.min)} F`
+
+            document.getElementById('dayThreeHumidity').innerHTML = `
+            H: ${weatherResults.daily[2].humidity}%`
+
+            // Day 4's weather
+            document.getElementById('imgFour').innerHTML = `
+            <img src="images/${weatherResults.daily[3].weather[0].icon}.svg" style="width:50px;height50px;" class="pt-2 pb-2" alt="${weatherResults.daily[3].weather[0].description} weather icon.">`
+
+            document.getElementById('dayFour').innerHTML = `<strong>${dayFour}</strong>`
+
+            document.getElementById('dateFour').innerHTML = `<p class="has-text-grey has-text-centered mb-1">${dateFour}</p>`
+
+            document.getElementById('dayFourTemp').innerHTML = `
+            ${Math.round(weatherResults.daily[3].temp.max)} / ${Math.round(weatherResults.daily[3].temp.min)} F`
+
+            document.getElementById('dayFourHumidity').innerHTML = `
+            H: ${weatherResults.daily[3].humidity}%`
+
+            // Day 5's weather
+            document.getElementById('imgFive').innerHTML = `
+            <img src="images/${weatherResults.daily[4].weather[0].icon}.svg" style="width:50px;height50px;" class="pt-2 pb-2" alt="${weatherResults.daily[4].weather[0].description} weather icon.">`
+
+            document.getElementById('dayFive').innerHTML = `<strong>${dayFive}</strong>`
+
+            document.getElementById('dateFive').innerHTML = `<p class="has-text-grey has-text-centered mb-1">${dateFive}</p>`
+
+            document.getElementById('dayFiveTemp').innerHTML = `
+            ${Math.round(weatherResults.daily[4].temp.max)} / ${Math.round(weatherResults.daily[4].temp.day)} F`
+
+            document.getElementById('dayFiveHumidity').innerHTML = `
+            H: ${weatherResults.daily[4].humidity}%`
+        })
+
+    })
+    
+    
+}
